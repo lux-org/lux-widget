effcfeb6623263317d2fd3da64986f558954a585
Caitlyn Chen <caitlynachen@gmail.com>
2020-08-16 20:55:21 -0700

    fix overflow current vis

--- src/selectableCard.tsx
+++ src/selectableCard.tsx
@@ -6,7 +6,7 @@ class SelectableCard extends React.Component<{selected:boolean,onClick:(any) => 
     var isSelected = this.props.selected ? "selected" : "";
     var className = "selectable " + isSelected;
     return (
-      <div className="card">
+      <div className="card" style={{display: 'inline-block'}}>
         <div className={className} onClick={this.props.onClick}>
           {this.props.children}
           <div className="check"><span className="checkmark">✔</span></div>
