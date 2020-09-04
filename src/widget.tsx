f6dc8b5ab28d979dd90f7901585815149a0b23db
Doris Lee <dorisjunglinlee@gmail.com>
2020-09-04 21:30:56 +0800

    support HTML rendering for warning messages, minor window close icon style change

--- src/widget.tsx
+++ src/widget.tsx
@@ -210,9 +210,9 @@ export class JupyterWidgetView extends DOMWidgetView {
                           className='fa fa-exclamation-triangle'
                           onClick={(e)=>this.openPanel(e)}/>;
           warnMsg = <div className="warning-footer" style={{visibility: (this.state.openWarning) ? 'visible' : 'hidden' }} >
-          <p className="warnMsgText"> {this.state.message} </p> 
+          <p className="warnMsgText" dangerouslySetInnerHTML={{__html: this.state.message}}></p> 
           <i className="fa fa-window-close" aria-hidden="true" onClick={(e)=>this.closePanel(e)}
-          style={{position: 'absolute', right: '15px', bottom: '12px', fontSize: '15px' }}
+          style={{position: 'absolute', right: '15px', fontSize: '15px' }}
           ></i> 
           </div>;
         }
