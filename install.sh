306badf18da4b128cbb2176379944ad53b5e3b79
dorisjlee <dorisjunglinlee@gmail.com>
2021-07-20 16:33:52 -0700

    recompile update to fix #94

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #npm install # run only for the first time 
 npx webpack # builds tsx --> ts --> js
+cp package.json luxwidget/nbextension/ # copy latest package.json over
 pip install . # builds python wheel and copies relevant js files to luxwidget/
 # When rebuild, these two lines don't need to be rerun since the code is symlinked
 jupyter nbextension install --sys-prefix --symlink --overwrite --py luxwidget
