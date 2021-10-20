18ba1cdf58be476dda8da6d1312d384db29b7ed2
Doris Lee <dorisjunglinlee@gmail.com>
2021-10-20 13:31:51 -0700

    [FIX] minor fix to install script

--- install.sh
+++ install.sh
@@ -1,4 +1,7 @@
 #npm install # run only for the first time 
+pip uninstall -y lux-widget
+jupyter nbextension uninstall luxwidget
+jupyter nbextension disable luxwidget
 npx webpack # builds tsx --> ts --> js
 cp package.json luxwidget/nbextension/ # copy latest package.json over
 pip install . # builds python wheel and copies relevant js files to luxwidget/
